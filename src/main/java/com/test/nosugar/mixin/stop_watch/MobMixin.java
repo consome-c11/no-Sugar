0c195df5877cbdcc2ec23619c674f2acfc1f358f
consome-c11 <test01106@outlook.jp>
2026-03-20 11:00:07 +0900

    無料食所:thinking:

---
+++ src/main/java/com/test/nosugar/mixin/stop_watch/MobMixin.java
@@ -0,0 +1,20 @@
+package com.test.nosugar.mixin.stop_watch;
+
+import com.test.nosugar.utils.TimeStopManager;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.Mob;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Mob.class)
+public class MobMixin {
+    @Inject(method = "serverAiStep", at= @At("HEAD"), cancellable = true)
+    private void nosugar$onserverAiStep(CallbackInfo ci){
+        Mob self = (Mob) (Object) this;
+        if(TimeStopManager.isStopped(self.level()) && !TimeStopManager.CanMove(self)) {
+            ci.cancel();
+        }
+    }
+}
